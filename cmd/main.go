b44c5af255bd4a3ffee9b7d81c291c9c86bae99d
whirlwinder <69021934+my-Sakura@users.noreply.github.com>
2021-07-03 15:44:40 +0800

    modify port

--- cmd/main.go
+++ cmd/main.go
@@ -25,5 +25,16 @@ func main() {
 
 	timeLineConn.RegistRouter(router.Group(timeLineRouterGroup))
 
-	log.Fatal(router.Run("0.0.0.0:10001"))
+	log.Fatal(router.Run("0.0.0.0:10002"))
 }
+
+docker volume create timeline-mysql-config
+docker volume create timeline-mysql-data
+docker volume create timeline-mysql-log
+docker run --name timeline -p 9092:3306 \ 
+  --mount type=volume,src=timeline-mysql-config,dst=/etc/mysql \
+  --mount type=volume,src=timeline-mysql-data,dst=/var/lib/mysql \
+  --mount type=volume,src=timeline-mysql-log,dst=/var/log/mysql \
+  -e MYSQL\_ROOT\_PASSWORD=123456 \
+	-d mysql:5.7
+	
